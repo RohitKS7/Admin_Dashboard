e89ec5b0660ed0027968fdf4d41072006b66ed1f
Rohit kumar suman <rohitkumarsuman2011india@gmail.com>
2023-01-03 11:18:04 +0530

    ei

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { BsCurrencyDollar } from "react-icons/bs";
+import { GoPrimitiveDot } from "react-icons/go";
+import { IoIosMore } from "react-icons/io";
+import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
+
+import { Stacked, Pie, Button, LineChart, SparkLine } from "../components";
+import {
+  earningData,
+  medicalproBranding,
+  recentTransactions,
+  weeklyStats,
+  dropdownData,
+  SparklineAreaData,
+  ecomPieChartData,
+} from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
+import product9 from "../data/product9.jpg";
 
 const Ecommerce = () => {
-  return <div>Ecommerce</div>;
+  const { activeMenu } = useStateContext();
+
+  return (
+    <div className="mt-12 ">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+        <div className="bg-white dark:text-gray-200  dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Ecommerce;
