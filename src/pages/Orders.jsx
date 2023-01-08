db75ab8f6c0608b71b64914b898844b39cbd6058
Rohit kumar suman <rohitkumarsuman2011india@gmail.com>
2023-01-08 10:50:26 +0530

    yo

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -21,6 +21,13 @@ const Orders = () => {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
+      <GridComponent>
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </GridComponent>
     </div>
   );
 };
