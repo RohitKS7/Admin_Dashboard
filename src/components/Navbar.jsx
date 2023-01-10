f0f06c3a78776f8283f6b730bd6bddbf62a9906f
Rohit Kumar Suman <rohitkumarsuman2011india@gmail.com>
2023-01-10 11:41:18 +0000

    Project Complete!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -91,7 +91,7 @@ const Navbar = () => {
             <p>
               <span className="text-gray-400 text-14">Hi,</span>{" "}
               <span className="text-gray-400 font-bold ml-1 text-14">
-                John Doe
+                Rohit
               </span>
             </p>
             <MdKeyboardArrowDown className="text-gray-400 text-14" />
--- src/components/index.js
+++ src/components/index.js
@@ -11,5 +11,4 @@ export { default as SparkLine } from "./Charts/SparkLine";
 export { default as LineChart } from "./Charts/LineChart";
 export { default as Stacked } from "./Charts/Stacked";
 export { default as Pie } from "./Charts/Pie";
-export { default as ChartsHeader } from "./ChartsHeader";
 export { default as Header } from "./Header";
